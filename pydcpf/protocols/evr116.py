8ccd331cf0e515d4b6780a12090d1722187b7c5e
Ondrej Grover <ondrej.grover@gmail.com>
2013-07-17 09:50:36 +0200

    bugfix: added response chars and chcek for 4 byte reposnse to 'p?' query

--- pydcpf/protocols/evr116.py
+++ pydcpf/protocols/evr116.py
@@ -1,7 +1,8 @@
 
 from . import base
 
-valid_command_characters = "hxyzijgpstv"
+valid_command_characters = "hxyzijgstv" # request chars
+valid_command_characters += "p" + valid_command_characters.upper() # response chars
 
 
 class RequestPacket(base.RequestPacket):
@@ -22,11 +23,17 @@ class RequestPacket(base.RequestPacket):
             possible_start = buffer_start
         else:
             possible_start += 2
-        for char in reversed(buffer(raw_packet, possible_start, end - possible_start - 1)):
+        possible_packet_buff = buffer(raw_packet, possible_start, end - possible_start - 1)
+        for char in reversed(possible_packet_buff):
             if char in valid_command_characters:
                 self.start = raw_packet.rfind(char, possible_start, end - 2)
                 self.length = end - self.start + 1
                 return True
+        # might be a reposnse to 'p?' query - only 4 bytes with no identifier
+        if len(possible_packet_buff) == 4:
+            self.start = possible_start
+            self.length = end - possible_start + 1
+            return True
         return False
 
 
