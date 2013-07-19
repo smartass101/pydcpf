c138941bf9f6f0c5697027387685c0e2786791ad
Ondrej Grover <ondrej.grover@gmail.com>
2013-07-19 17:28:47 +0200

    check for available EVR116 position range

--- pydcpf/appliances/evr116.py
+++ pydcpf/appliances/evr116.py
@@ -15,13 +15,15 @@ class Device(core.Device):
         super(Device, self).__init__(address=address, protocol_module='pydcpf.protocols.evr116', *kwargs)
 
     def set_position(self, position):
+        if position < 512 or position > 6760:
+            raise ValueError("position not in range [512, 6760]")
         hex_position = "%x" % (position / 2)
         return self.query(IDENTIFIER='g', DATA=hex_position)
 
 
     def get_position(self):
         hex_position = self.query(IDENTIFIER='p', DATA='?')
-        return int(hex_position, 16)
+        return int(str(hex_position), 16)
 
 
     def close_valve(self):
